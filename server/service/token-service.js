b9022ca337f19efd6cba544179414eadc123b623
Taras Leshchuk <tarasleshchuk01@gmail.com>
2022-08-04 14:33:21 +0300

    commit

---
+++ server/service/token-service.js
@@ -0,0 +1,52 @@
+const jwt = require('jsonwebtoken')
+const TokenModel = require('../models/token-model')
+
+class TokenService {
+    generateToken(payload) {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' })
+
+        return { accessToken, refreshToken }
+    }
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+            return userData
+        } catch {
+            return null
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+            return userData
+        } catch {
+            return null
+        }
+    }
+
+    async saveToken(userId, refreshToken) {
+        const tokenData = await TokenModel.findOne({ user: userId })
+
+        if (tokenData) {
+            tokenData.refreshToken = refreshToken
+            return tokenData.save()
+        }
+
+        await TokenModel.create({ user: userId, refreshToken })
+    }
+
+    async removeToken(refreshToken) {
+        const tokenData = await TokenModel.deleteOne({ refreshToken })
+        return tokenData
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await TokenModel.findOne({ refreshToken })
+        return tokenData
+    }
+}
+
+module.exports = new TokenService()
